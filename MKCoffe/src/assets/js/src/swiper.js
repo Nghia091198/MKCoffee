4baea4aa8733802239adf5e328da198a8b52aa10
AnhNghia911 <anhnghia0998@gmail.com>
2020-09-22 21:44:38 +0700

    done page san pham

--- MKCoffe/src/assets/js/src/swiper.js
+++ MKCoffe/src/assets/js/src/swiper.js
@@ -1,10 +1,20 @@
-
-    var swiper = new Swiper('.swiper-container', {
-      pagination: {
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
         el: '.swiper-pagination',
         clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
         },
-      },
-    });
+    },
+});
+
+// San Pham
+var swiper = new Swiper('.swiper-sp', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    },
+});
