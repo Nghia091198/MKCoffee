06ba8422a11ddf99b71742fdf90d83e558ce51f1
AnhNghia911 <anhnghia0998@gmail.com>
2020-09-26 10:59:09 +0700

    add

--- MKCoffe/dest/assets/js/src/main.js
+++ MKCoffe/dest/assets/js/src/main.js
@@ -4,12 +4,12 @@ var frmSearch = document.querySelector(".section__search");
 var frm = document.querySelector(".box__search");
 var btnClose = document.querySelector(".btn-close");
 btnOpen.onclick = function() {
-    frmSearch.classList.add('active');
+    frmSearch.classList.add('active-1');
     frm.style.width = "100%";
 };
 
 btnClose.onclick = function() {
-    frmSearch.classList.remove('active');
+    frmSearch.classList.remove('active-1');
     frm.style.width = "0";
 };
 
@@ -22,16 +22,16 @@ var btnClose = document.querySelector(".section-back");
 var content = document.querySelector(".navigation-box")
 btnNav.onclick = function() {
     navMenu.classList.add("active-3");
-    content.classList.add("active");
+    content.classList.add("active-1");
     pushLeft.classList.add("active-2");
-    btnClose.classList.add("active")
+    btnClose.classList.add("active-1")
 };
 
 btnClose.onclick = function() {
     navMenu.classList.remove("active-3");
     pushLeft.classList.remove("active-2");
-    content.classList.remove("active");
-    btnClose.classList.remove("active");
+    content.classList.remove("active-1");
+    btnClose.classList.remove("active-1");
 }
 
 // Collapse
@@ -54,15 +54,15 @@ var coll = document.getElementsByClassName("btn-collapse");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active-coll");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-        content.style.maxHeight = null;
-      } else {
-         content.style.maxHeight = "100%";
-      } 
-  });
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active-coll");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = "100%";
+        }
+    });
 }
 
 // Full Page
